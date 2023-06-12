6377dda868ddad401965b01ce0ffa7bda87b9c96
chidiebereojingwa <ojingwaoji@gmail.com>
2023-06-12 18:31:44 +0100

    made changes

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-numbers = [3, 6, 800, 4, 9, 10]
-highest = 0
-for num in numbers:
-    if num > highest:
-        highest = num
-print(highest)
+phone = input("Phone: ")
+number_dict = {
+    "1": "One",
+    "2" : "Two",
+    "3" : "Three",
+    "4" : "Four"
+}
+num_word = ""
+for ch in phone:
+    num_word += number_dict.get(ch, "!") + " "
+print(num_word)
