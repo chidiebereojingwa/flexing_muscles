90481e0ec4fc329f1e9377c1de38d23afe5748b8
chidiebereojingwa <ojingwaoji@gmail.com>
2023-06-14 01:32:16 +0100

    made changes

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-phone = input("Phone: ")
-number_dict = {
-    "1": "One",
-    "2" : "Two",
-    "3" : "Three",
-    "4" : "Four"
-}
-num_word = ""
-for ch in phone:
-    num_word += number_dict.get(ch, "!") + " "
-print(num_word)
+# phone = input("Phone: ")
+# number_dict = {
+#     "1": "One",
+#     "2" : "Two",
+#     "3" : "Three",
+#     "4" : "Four"
+# }
+# num_word = ""
+# for ch in phone:
+#     num_word += number_dict.get(ch, "!") + " "
+# print(num_word)
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print(f"Hi, is me {self.name}")
+
+
+john = Person('Bob smith')
+john.talk()
